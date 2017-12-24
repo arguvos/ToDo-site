f648859ee93878e1c2eb87901190fae80a7d3cb0
Vasily Palkov <vasilypalkov@gmail.com>
2017-12-25 01:40:30 +0400

    Added registration.

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,9 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
+
 from polls import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    #path('all/', views.index, name='index'),
+    
+    url(r'^$', views.home, name='home'),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^all/$', views.index, name='index'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
 from .models import Task
 
 def index(request):
@@ -8,3 +17,22 @@ def index(request):
     return render(request, "index.html", {
             'tasks': tasks
         })
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+    
